fbae33c59cb455bc4359c93c47eeee868200369d
Matii326 <45210981+Matii326@users.noreply.github.com>
2024-04-06 18:36:21 +0200

    zadanie 4

--- main.java
+++ main.java
@@ -1,16 +1,27 @@
+import geometry.Circle;
 import geometry.ColoredCircle;
 import geometry.Point;
 
 class Main {
   public static void main(String[] args) {
-    Point p = new Point(2.45, 5.76);
-    ColoredCircle c = new ColoredCircle(p, 5.2, "Red");
+    Circle[] circles = {
+        new Circle(new Point(2.4, 5.2), 3),
+        new ColoredCircle(new Point(3.5, 2), 4.12, "Blue"),
+        new Circle(new Point(3.4, 1.2), 2.12),
+        new Circle(new Point(2.8, 5.7), 7.67),
+        new ColoredCircle(new Point(3.9, 1.9), 5.12, "Green"),
+        new Circle(new Point(9.12, 12.9), 15.6),
+        new ColoredCircle(new Point(10.2, 2.1), 3.14, "Red"),
+    };
 
-    System.out.println("Wlasciwosci kola:");
-    System.out.println(c.getArea());
-    System.out.println(c.calculatePerimeter());
-    System.out.println(c.getCenter().getX());
-    System.out.println(c.getCenter().getY());
-    System.out.println(c.getColor());
+    for (Circle circle : circles) {
+      if (circle instanceof ColoredCircle) {
+        System.out.println(
+            String.format("Pole: %f %s", circle.getArea(), ((ColoredCircle) circle).getColor()));
+      } else {
+        System.out.println(
+            String.format("Pole: %f", circle.getArea()));
+      }
+    }
   }
 }
